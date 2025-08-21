fbac72a932608feb5c9bd09d658edc418927b7c1
aryagama <arya201001@gmail.com>
2025-08-22 03:12:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -264,8 +264,7 @@ moodBtn.textContent = `MOOD (${moodLabels[currentMoodIndex]})`;
 // Receipt data with names
 const strukFiles = [
   { name: "Struk 1", file: 'Struk 1_Trial.png' },
-  { name: "Struk 2", file: 'Struk 2.png' },
-  { name: "Struk 4", file: 'Struk 4.png' },
+  { name: "Struk 2", file: 'Struk 2_Trial.png' },
 ];
 
 // Generate struks in a grid
